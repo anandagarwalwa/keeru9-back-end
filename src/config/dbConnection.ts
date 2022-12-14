6051af56902c67724abf28b1e693e4cde9d3070d
anandagarwalwa <61784483+anandagarwalwa@users.noreply.github.com>
2022-12-14 14:22:32 +0530

    Update dbConnection.ts

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -4,7 +4,7 @@ dotenv.config()
 import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(process.env.DB_NAME ?? "keeru9", process.env.DB_USER ?? "root", process.env.DB_PASSWORD ?? "", {
-    host: process.env.DB_HOST ?? "localhost",
+    host: process.env.DB_HOST ?? "3.19.97.191",
     dialect: 'mysql',
     port: parseInt(process.env.DB_PORT ?? "3306")
 });
